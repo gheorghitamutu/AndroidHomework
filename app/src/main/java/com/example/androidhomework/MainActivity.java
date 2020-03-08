f57f9634737524b6aec97e19954cf1366a3ea78d
Gheorghita Mutu <gmutu@bitdefender.com>
2020-03-08 10:36:18 +0200

    added 2nd homework tasks

--- app/src/main/java/com/example/androidhomework/MainActivity.java
+++ app/src/main/java/com/example/androidhomework/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.androidhomework;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
+
+//https://stackoverflow.com/questions/13022677/save-state-of-activity-when-orientation-changes-android
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    final int productCount = 4;
+    final private String productTextViewValueContent = "productTextViewValueContent";
     ListView listView;
+    TextView productTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         final ArrayList<String> products = new ArrayList<>();
 
-        for(int i = 0; i < 4; i++) {
+        for (int i = 0; i < productCount; i++) {
             products.add(String.format(Locale.ENGLISH, "Product 0%d", i));
         }
 
@@ -32,14 +44,65 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
 
+        // was this just rotated? check savedInstanceState value!
+        productTextView = findViewById(R.id.product_view);
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(productTextViewValueContent)) {
+                String content = savedInstanceState.getString(productTextViewValueContent);
+                productTextView.setText(content);
+            }
+        }
+
         // show the selected item
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String clickedItem = (String) listView.getItemAtPosition(position);
-                final TextView productTextView = findViewById(R.id.product_view);
                 productTextView.setText(clickedItem);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(productTextViewValueContent, productTextView.getText());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
